40a0bf74e3234e7969c7ae2e18ed69a73e1aff4a
林佳志乃 <twinklestaryoshino@gmail.com>
2018-06-16 16:54:32 +0900

    ヘッダーにグループページリンク作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "top#index"
   resources :users, only: [:show, :edit, :update]
+  resources :groups, only: [:show, :edit, :update]
 
 end
